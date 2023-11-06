a0e9f4527af92b5a9935440abc45f66a462cf189
Piotr Baranowski <piotr.baranowski250@gmail.com>
2023-11-06 15:52:15 +0100

    Basic skeleton working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [activities, setActivites] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivites(response.data)
+      })
+  },[]) 
 
   return (
     <div>
       <h1>Reactivites</h1>
+      <ul>
+        {activities.map((activity:any) => (
+          <li key={activity.id}>
+            {activity.title}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
